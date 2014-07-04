6843e58fd90d1a76aae37843c4b88f058054f2cb
ewancoder <ewancoder@gmail.com>
2014-07-04 22:20:37 +0300

    Fixed notification (at least kind of)

--- .wmii-hg/settings.py
+++ .wmii-hg/settings.py
@@ -34,8 +34,8 @@ d.startup("~/bin/updates")
 
 #Background
 d.startup("~/bin/pic ~/Dropbox/Pictures 300")
-#Run RSS curses-based client
-d.startup("urxvt -name Canto -e canto-curses -a $IP_RPI -p 7070")
+#Run SSH-CANTO client
+d.startup('~/bin/canto')
 
 #Initialize wifi lan interface
 d.startup("sudo ifconfig lan up 192.168.1.1 netmask 255.255.255.0")
