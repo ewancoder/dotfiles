1e09bf4cee67e60ffb50792d0f4724b96f928acb
ewancoder <ewancoder@gmail.com>
2014-07-29 17:51:49 +0300

    Refactored WMII config + added zsh-alias to quit wmii

--- .wmii-hg/settings.py
+++ .wmii-hg/settings.py
@@ -1,47 +1,74 @@
-#!/usr/bin/env python
-import data as d
+#========== GENERAL FUNCTIONS ==========
+
+global blocks
+blocks = []
+global colRules, tagRules
+colRules, tagRules = '', ''
+global text, name, check, lower, bigger, mid, color
+text, name, check, lower, bigger, mid, color = '', '', '', '', '', '', ''
+
+def addBlock():
+    global blocks
+    global text, name, check, lower, bigger, mid, color
+    x = len(blocks)
+    #Generate name for block
+    if x < 10:
+        name = 'Status_a' + str(x)
+    else:
+        name = 'Status_b' + str(x) #This is it, noone needs 20+ statusbars
+    #Create new block
+    blocks.append([text, name, check, lower, bigger, mid, color])
+    text, name, check, lower, bigger, mid, color = '', '', '', '', '', '', ''
+
+#Function forming colrules
+def addColRule(col, rule):
+    global colRules
+    colRules = colRules + '/' + str(col) + '/ -> ' + str(rule) + '\n'
+
+#Function forming tagrules
+def addTagRule(tag, forcetag):
+    global tagRules
+    tagRules = tagRules + '/' + str(tag) + '/ force-tags=' + str(forcetag) + '\n'
 
 #========== CONFIGURATION ==========
 
 #=== COLORS ===
 #Color of (not)focused tag/window
-NormColors = "#ada #000 #000"
-FocusColors = "#add #345 #000"
-TagFocusColors = "#fc5 #862 #000"
+normColors = '#ada #000 #000'
+focusColors = '#add #345 #000'
+tagFocusColors = '#fc5 #862 #000'
 #Good, Mid & Bad colors for checking state and drawing status
-GoodColors = "#ada #350 #000"
-MidColors = "#dda #640 #000"
-BadColors = "#daa #600 #000"
+goodColors = '#ada #350 #000'
+midColors = '#dda #640 #000'
+badColors = '#daa #600 #000'
 #Color for mounted devices
-DeviceColors = "#ada #b42 #000"
-
-d.normcolors = NormColors
-d.focuscolors = FocusColors
+deviceColors = '#ada #b42 #000'
+#Color for unstaged/unpushed/uncommited git repos
+gitColors = '#f63 #000 #000'
 
 #=== GENERAL CONFIG ===
-d.modkey = "Mod4"
-d.up = "k"
-d.down = "j"
-d.left = "h"
-d.right = "l"
+modkey = 'Mod4'
+up = 'k'
+down = 'j'
+left = 'h'
+right = 'l'
 
-d.font = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*"
-d.term = "urxvt"
-d.border = 1
+font = '-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*'
+term = 'urxvt'
+border = 1
 
 #Panel position (top or bottom)
-position = "top"
+position = 'top'
 
 #Background folder
-background = "~/Dropbox/Pictures"
+background = '~/Dropbox/Pictures'
 #Timeout to change background, in seconds
 timeout = 300
 
 #Check for Arch Linux updates each N seconds
 updatesTimeout = 600
 
-#Copy starts here because I need it only for backup (it can be killed upon X11 destruction) and systemd isn't working for copy-agent@ewancoder
-#Dropbox is a systemd service which runs all the time 'ewancoder' is logged
+#Startup X11 apps
 startup = [
     'gxkb',
     'CopyAgent',
@@ -49,101 +76,101 @@ startup = [
     '~/bin/ircnotify',
     'tilda'
 ]
+#Startup as via bash (won't be killed upon X11 termination)
 rawstartup = [
     'chromium --no-startup-window'
 ]
 
 #=== COLUMN RULES ===
 #For skype mini-window on the left side
-d.addColRule(0, "20+80")
+addColRule(0, "20+80")
 #For two Thunars / 2 chromium windows
-d.addColRule(2, "50+50")
+addColRule(2, "50+50")
 #For anything else - Golden Ratio
-d.addColRule(".*", "62+38")
+addColRule(".*", "62+38")
 
 #=== TAGGING RULES ===
 #For canto-curses urxvt 'Canto' window
-d.addTagRule("Canto", "RSS")
+addTagRule("Canto", "RSS")
 #For Skype to be at 0 :)
-d.addTagRule("Skype", 0)
+addTagRule("Skype", 0)
 #For separate steam big-picture tag
-d.addTagRule("Steam", "Steam")
+addTagRule("Steam", "Steam")
 
 #=== STATUSBAR ===
 #Time
-d.time = "date +%a\\ %b\\ %d\\ %T\\"
+time = "date +%a\\ %b\\ %d\\ %T\\"
 #Free RAM
-d.text = "echo $(free -mh | grep /cache | awk '{print $4}') 'M'"
-d.check = "free -m | grep /cache | awk '{print $4}'"
-d.bigger = 3000
-d.mid = 1000
-d.addBlock()
+text = "echo $(free -mh | grep /cache | awk '{print $4}') 'M'"
+check = "free -m | grep /cache | awk '{print $4}'"
+bigger = 3000
+mid = 1000
+addBlock()
 #Space at /
-d.text = "echo $(df -h / | grep / | awk '{print $5}' | sed 's/%//') '/'"
-d.check = "df -h / | grep / | awk '{print $5}' | sed 's/%//'"
-d.lower = 45
-d.mid = 75
-d.addBlock()
+text = "echo $(df -h / | grep / | awk '{print $5}' | sed 's/%//') '/'"
+check = "df -h / | grep / | awk '{print $5}' | sed 's/%//'"
+lower = 45
+mid = 75
+addBlock()
 #Space at /home
-d.text = "echo $(df -h /home | grep home | awk '{print $5}' | sed 's/%//') 'H'"
-d.check = "df -h /home | grep home | awk '{print $5}' | sed 's/%//'"
-d.lower = 45
-d.mid = 75
-d.addBlock()
+text = "echo $(df -h /home | grep home | awk '{print $5}' | sed 's/%//') 'H'"
+check = "df -h /home | grep home | awk '{print $5}' | sed 's/%//'"
+lower = 45
+mid = 75
+addBlock()
 #Space at /mnt/cloud
-d.text = "echo $(df -h /mnt/cloud | grep cloud | awk '{print $5}' | sed 's/%//') 'C'"
-d.check = "df -h /mnt/cloud | grep cloud | awk '{print $5}' | sed 's/%//'"
-d.lower = 45
-d.mid = 75
-d.addBlock()
+text = "echo $(df -h /mnt/cloud | grep cloud | awk '{print $5}' | sed 's/%//') 'C'"
+check = "df -h /mnt/cloud | grep cloud | awk '{print $5}' | sed 's/%//'"
+lower = 45
+mid = 75
+addBlock()
 #Space at /mnt/backup
-d.text = "echo $(df -h /mnt/backup | grep backup | awk '{print $5}' | sed 's/%//') 'B'"
-d.check = "df -h /mnt/backup | grep backup | awk '{print $5}' | sed 's/%//'"
-d.lower = 45
-d.mid = 75
-d.addBlock()
+text = "echo $(df -h /mnt/backup | grep backup | awk '{print $5}' | sed 's/%//') 'B'"
+check = "df -h /mnt/backup | grep backup | awk '{print $5}' | sed 's/%//'"
+lower = 45
+mid = 75
+addBlock()
 #CPU Temperature
-d.text = "cat /sys/class/hwmon/hwmon0/device/temp1_input | cut -c1-2"
-d.check = d.text
-d.lower = 40
-d.mid = 50
-d.addBlock()
+text = "cat /sys/class/hwmon/hwmon0/device/temp1_input | cut -c1-2"
+check = text
+lower = 40
+mid = 50
+addBlock()
 #CPU Frequency
-d.text = "cat /proc/cpuinfo | grep -m 1 MHz | awk '{printf \"%.0f\\n\", $4}'"
-d.addBlock()
+text = "cat /proc/cpuinfo | grep -m 1 MHz | awk '{printf \"%.0f\\n\", $4}'"
+addBlock()
 #GPU Temperature
-d.text = "nvidia-settings -q=GPUCoreTemp | grep -m 1 ewanhost | awk '{print $4}' | cut -c1-2"
-d.check = d.text
-d.lower = 40
-d.mid = 52
-d.addBlock()
+text = "nvidia-settings -q=GPUCoreTemp | grep -m 1 ewanhost | awk '{print $4}' | cut -c1-2"
+check = text
+lower = 40
+mid = 52
+addBlock()
 #Sound Volume
-d.text = "amixer | grep \"Left: Playback\" | awk {'print $5'} | cut -d \"[\" -f2 | cut -d \"%\" -f1"
-d.color = FocusColors
-d.addBlock()
+text = "amixer | grep \"Left: Playback\" | awk {'print $5'} | cut -d \"[\" -f2 | cut -d \"%\" -f1"
+color = focusColors
+addBlock()
 #NETSTATS
-d.text = "~/.wmii-hg/netmon"
-d.color = FocusColors
-d.addBlock()
+text = "~/.wmii-hg/netmon"
+color = focusColors
+addBlock()
 #CPU Uptime
-d.text = "uptime | sed 's/.*://; s/, / /g'"
-d.check = d.text + " | awk '{print $2}'"
-d.color = FocusColors
-d.lower = 1.75
-d.addBlock()
+text = "uptime | sed 's/.*://; s/, / /g'"
+check = text + " | awk '{print $2}'"
+color = focusColors
+lower = 1.75
+addBlock()
 
 #Output for wmiirc
 if __name__ == "__main__":
-    print(d.modkey)
-    print(d.up)
-    print(d.down)
-    print(d.left)
-    print(d.right)
+    print(modkey)
+    print(up)
+    print(down)
+    print(left)
+    print(right)
 
-    print(NormColors)
-    print(FocusColors)
-    print(TagFocusColors)
-    print(GoodColors)
+    print(normColors)
+    print(focusColors)
+    print(tagFocusColors)
 
-    print(d.font)
-    print(d.term)
+    print(font)
+    print(term)
--- .wmii-hg/wmii.py
+++ .wmii-hg/wmii.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 import os
 import re
-import settings as data
-import shlex
+import settings
 import subprocess
 import threading
-from time import sleep
 
 #Mask for removing \n\t\r from bash get() function result
 mask = re.compile('[\n\t\r]')
@@ -14,31 +12,17 @@ mask = re.compile('[\n\t\r]')
 
 #Get result from bash command
 def get(command):
-    return mask.sub("", os.popen(command).read())
+    return mask.sub('', os.popen(command).read())
 
-#========== BLOCK FUNCTIONS ==========
+#Just os.system but with '&'
+def run(command):
+    os.system(command + ' > /dev/null 2>&1 &')
 
-#Create block
-def createBlock(name):
-    os.system('wmiir create /rbar/'+name+' &')
+#========== SET FUNCTIONS ==========
 
-#Set block text
-def setStatus(name, text):
-    os.system('wmiir xwrite /rbar/'+name+' label "'+text+'"')
-
-#Set block color
-def setColor(name, color):
-    os.system('wmiir xwrite /rbar/'+name+' colors "'+color+'"')
-
-#Check condition
-def checkCondition(name, check, lower, bigger, mid, color, midColor, badColor):
-    if color == "":
-        color = data.GoodColors
-    if midColor == "":
-        midColor = data.MidColors
-    if badColor == "":
-        badColor = data.BadColors
-    if check != "":
+#Set condition (Good/Middle/Bad) for a block
+def setCondition(name, check, lower, bigger, mid, color):
+    if check != '':
         try:
             checked = float(get(check))
         except:
@@ -46,80 +30,65 @@ def checkCondition(name, check, lower, bigger, mid, color, midColor, badColor):
             pass
         if bigger != "":
             if checked > float(bigger):
-                setColor(name, color)
+                setColor(name, settings.goodColors)
             elif (mid != "") and (checked > float(mid)):
-                setColor(name, midColor)
+                setColor(name, settings.midColors)
             else:
-                setColor(name, badColor)
+                setColor(name, settings.badColors)
         else:
             if checked < float(lower):
-                setColor(name, color)
+                setColor(name, settings.goodColors)
             elif (mid != "") and (checked < float(mid)):
-                setColor(name, midColor)
+                setColor(name, settings.midColors)
             else:
-                setColor(name, badColor)
+                setColor(name, settings.badColors)
     else:
-        setColor(name, color)
+        if color != '':
+            setColor(name, color)
+        else:
+            setColor(name, settings.goodColors)
 
-#========== RULES FUNCTIONS ==========
+#Set column & tagging rules
+def setRules():
+    run("wmiir write /colrules <<!\n" + settings.colRules + "!")
+    run("wmiir write /rules <<!\n" + settings.tagRules + "!")
 
-#Create column & tagging rules
-def makeRules():
-    os.system("wmiir write /colrules <<!\n" + data.d.colRules + "!")
-    os.system("wmiir write /rules <<!\n" + data.d.tagRules + "!")
+#========== BLOCK FUNCTIONS ==========
 
-def checkrss():
-    if os.path.isfile('/tmp/rssitems'):
-        os.system('wmiir xwrite /lbar/RSS colors "'+data.GoodColors+'"')
-        os.system('rm /tmp/rssitems')
+#Create block
+def createBlock(name):
+    run('wmiir create /rbar/' + name)
 
-def checkcv():
-    status = get('cv | grep % | awk \'{for(i=1;i<=NF;i++) if ($i ~/%$/) {print $i+0} {print " "}}\'')
-    if status != '':
-        createBlock('Processing')
-        setColor('Processing', data.MidColors)
-        setStatus('Processing', status)
-    else:
-        os.system('wmiir rm /rbar/Processing')
+#Set block text
+def setStatus(name, text):
+    run('wmiir xwrite /rbar/' + name + ' label "' + text + '"')
 
-def checkmedia():
-    media = get('ls -1 /media | tr "\\n" " "')
-    if media != '':
-        createBlock('Devices')
-        setColor('Devices', data.DeviceColors)
-        setStatus('Devices', media)
-    else:
-        os.system('wmiir rm /rbar/Devices')
-
-def checkgit():
-    repos = get('~/bin/gitch | xargs -L 1 basename | tr "\\n" " "')
-    if repos != '':
-        createBlock('AGitCheck')
-        setColor('AGitCheck', "#f63 #000 #000")
-        setStatus('AGitCheck', repos)
-    else:
-        os.system('wmiir rm /rbar/AGitCheck')
+#Set block color
+def setColor(name, color):
+    run('wmiir xwrite /rbar/' + name + ' colors "' + color + '"')
 
 #Create all blocks
 def makeBlocks():
-    for x in data.d.block:
-        if x[2] != "":
-            color = x[2]
-        else:
-            color = data.GoodColors
-        createBlock(x[0])
+    for x in settings.blocks:
+        createBlock(x[1])
     #CreateTime
     createBlock("Time")
 
-#Set text for all blocks
+#Set text&color for all blocks
 def statusBlocks():
-    for x in data.d.block:
-        setStatus(x[0], get(x[1]))
+    for x in settings.blocks:
+        setStatus(x[1], get(x[0]))
+        setCondition(x[1], x[2], x[3], x[4], x[5], x[6])
 
-#Check all blocks and apply color
-def colorBlocks():
-    for x in data.d.block:
-        checkCondition(x[0], x[3], x[4], x[5], x[6], x[2], x[7], x[8])
+#Creates a block (name) if status != ''
+def check(status, name, color):
+    currentStatus = get(status)
+    if currentStatus != '':
+        createBlock(name)
+        setColor(name, color)
+        setStatus(name, currentStatus)
+    else:
+        run('wmiir rm /rbar/' + name)
 
 #========== STARTUP FUNCTIONS ==========
 
@@ -127,68 +96,67 @@ pids = []
 
 #Startup function
 def startup():
-    for command in data.startup:
+    for command in settings.startup:
         pids.append(subprocess.Popen(command, shell = True).pid)
-    for command in data.rawstartup:
-        os.system(command + '&')
+    for command in settings.rawstartup:
+        run(command)
 
 def killAll():
     for pid in pids:
-        os.system('kill ' + str(pid))
+        run('kill ' + str(pid))
 
-#========== EVENTS FUNCTIONS ==========
-
-#Events handling & eventloop
-def eventloop():
-    os.system("./oldwmii")
-
-#========== MAIN FUNCTION ==========
+#========== LOOP FUNCTIONS ==========
 
 def loopStatusBar():
     threading.Timer(2.0, loopStatusBar).start()
-    colorBlocks()
     statusBlocks()
-    checkrss()
-    checkcv()
-    checkmedia()
-    checkgit()
-    os.system("~/.wmii-hg/mypo &")
+    #Check for cp (cv) running and show percentage
+    check('cv | grep % | awk \'{for(i=1;i<=NF;i++) if ($i ~/%$/) {print $i+0} {print " "}}\'', 'Processing', settings.midColors)
+    #Check for removable media mounted and show them
+    check('ls -1 /media | tr "\\n" " "', 'Devices', settings.deviceColors)
+    #Check for git repos unstaged/unpushed/uncommited and show them
+    check('~/bin/gitch | xargs -L 1 basename | tr "\\n" " "', 'AGitCheck', settings.gitColors)
+    #Check for pulseaudio sinks/sources and show them
+    run("~/.wmii-hg/mypo")
 
 def loopTime():
     threading.Timer(1.0, loopTime).start()
-    if os.system('wmiir ls /') != 0:
+    if os.system('wmiir ls / > /dev/null 2>&1') != 0:
         killAll()
+        print('Exiting wmii, bye and good luck!')
         os._exit(1)
-    setColor("Time", data.FocusColors)
-    setStatus("Time", get(data.d.time))
+    setColor("Time", settings.focusColors)
+    setStatus("Time", get(settings.time))
 
 def loopBackground():
-    threading.Timer(data.timeout, loopBackground).start()
-    os.system('feh --bg-fill --randomize --recursive ' + data.background)
+    threading.Timer(settings.timeout, loopBackground).start()
+    run('feh --bg-fill --randomize --recursive ' + settings.background)
 
 def loopSysUpdate():
-    threading.Timer(data.updatesTimeout, loopSysUpdate).start()
+    threading.Timer(settings.updatesTimeout, loopSysUpdate).start()
     upd = get('yaourt -Qua')
     num = get('yaourt -Qua | wc -l')
     if num != '0':
-        os.system('yaourt -Qua > /tmp/yaourt.updates && notify-send -u low "Updates available (' + num + ')" "$(cat /tmp/yaourt.updates)"')
+        run('yaourt -Qua > /tmp/yaourt.updates && notify-send -u low "Updates available (' + num + ')" "$(cat /tmp/yaourt.updates)"')
 
 def main():
     #Before all of this - we need to set background instead of ugly gray color
     loopBackground()
+    #Set position
+    run("wmiir xwrite /ctl bar on " + settings.position)
+    #Run tray
+    run("witray")
     #Handle Time
     loopTime()
     #Handle Rules
-    makeRules()
-    #Set position
-    os.system("wmiir xwrite /ctl bar on " + data.position)
-    #Update system check
-    loopSysUpdate()
-    #After handling rules, Startup
-    startup()
+    setRules()
     #Handle StatusBar
     makeBlocks()
     loopStatusBar()
+    #Update system check
+    loopSysUpdate()
+    #After all, Startup
+    startup()
 
 if __name__ == "__main__":
     main()
