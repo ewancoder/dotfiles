7e87ba62b7a54fda0a03550d32e59e2487b3fe4a
ewancoder <ewancoder@gmail.com>
2014-10-28 04:47:05 +0300

    Added voximp config

---
+++ .config/voximp/voximpconf.py
@@ -0,0 +1,20 @@
+languagemodel = '9000'
+
+keycommand = {
+    'TERMINAL': "super+Return",
+    'CLOSE': "ctrl+q",
+    'ENTER': "Return",
+    'PASTE': "shift+insert"
+}
+programcommand = {
+    'INTERNET': "chromium",
+    'OPEN GVIM': "gvim",
+    'GOOGLE': "chromium www.google.com",
+}
+mousecommand = {
+    'CLICK': '1',
+    'RIGHTCLICK': '3'
+}
+confirm = [
+    'CLOSE'
+]
