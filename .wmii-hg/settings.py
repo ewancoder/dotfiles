28a76f1592e8e3ce6722a1773640c5a80dcee59d
ewancoder <ewancoder@gmail.com>
2015-01-16 17:53:06 +0300

    t

--- .wmii-hg/settings.py
+++ .wmii-hg/settings.py
@@ -99,8 +99,6 @@ addColRule(".*", "62+38")
 addTagRule("Skype", 0)
 #For separate steam big-picture tag
 addTagRule("Steam", "Steam")
-#Popcorntime
-addTagRule("Popcorn-Time", "sel+9")
 #VLC
 addTagRule("vlc", "sel+0")
 
