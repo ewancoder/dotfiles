6caaec487e43ebf89c437471197e14c49e591817
Ivan Zyranau <ewancoder@gmail.com>
2025-02-09 21:20:24 +0400

    Ignore certificates for connecting to Win RDP

--- .local/bin/rdpwin.sh
+++ .local/bin/rdpwin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 for i in 1 2 3 4 5; do
-xfreerdp3 /v:192.168.2.2 /u:'Ivan Zyranau' /d:'' /p:$RDP_PASSWORD +compression +clipboard +fonts /dynamic-resolution +async-channels +async-update +auto-reconnect /bpp:16 /gfx:progressive /gfx && break; done
+xfreerdp3 /cert:ignore /v:192.168.2.2 /u:'Ivan Zyranau' /d:'' /p:$RDP_PASSWORD +compression +clipboard +fonts /dynamic-resolution +async-channels +async-update +auto-reconnect /bpp:16 /gfx:progressive /gfx && break; done
