3272e260c7fcafb80fa8255e3011fa85a76f08a4
Ivan Zyranau <ewancoder@gmail.com>
2025-03-05 17:48:34 +0400

    Connect to rdp over wifi after all

--- .local/bin/rdpwin.sh
+++ .local/bin/rdpwin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xfreerdp3 /audio-mode:0 /cert:ignore /auth-pkg-list:'!kerberos' /v:192.168.2.2 /u:"$RDP_USERNAME" /d:'' /p:"$RDP_PASSWORD" +compression +clipboard +fonts /dynamic-resolution +async-channels +async-update +auto-reconnect /bpp:16 /gfx:progressive /gfx
+xfreerdp3 /audio-mode:0 /cert:ignore /auth-pkg-list:'!kerberos' /v:192.168.1.185 /u:"$RDP_USERNAME" /d:'' /p:"$RDP_PASSWORD" +compression +clipboard +fonts /dynamic-resolution +async-channels +async-update +auto-reconnect /bpp:16 /gfx:progressive /gfx
