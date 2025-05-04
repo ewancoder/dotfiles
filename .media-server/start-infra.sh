c7c04856bf19b6be2bd00f8957a2ed621d430297
Ivan Zyranau <ewancoder@gmail.com>
2025-05-04 06:33:20 +0400

    Add RedisInsights and Doneman (for overlay networks)

--- .media-server/start-infra.sh
+++ .media-server/start-infra.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker compose -f infra.yml -p typingrealm-infra up -d
+docker compose -f infra.yml -p typingrealm-infra cp infra-networks.yml doneman:/config.yml
+docker compose -f infra.yml -p typingrealm-infra restart doneman
