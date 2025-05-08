e6a828e440339214a6008102f208a2c4a05ce003
Ivan Zyranau <ewancoder@gmail.com>
2025-05-08 20:27:55 +0400

    Backup everything to HDD as well

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-rsync -av /mnt/data/media/configs/ /mnt/backup/backups/media-configs
-rsync -av /mnt/data/seq/ /mnt/backup/backups/seq
-rsync -av /mnt/data/unique/ /mnt/backup/backups/unique
+rsync -av --delete /mnt/data/media/configs/ /mnt/backup/backups/media-configs
+rsync -av --delete /mnt/data/seq/ /mnt/backup/backups/seq
+rsync -av --delete /mnt/data/unique/ /mnt/backup/backups/unique
+
+rsync -av --delete /mnt/data/media/configs/ /mnt/hdd/backups/media-configs
+rsync -av --delete /mnt/data/seq/ /mnt/hdd/backups/seq
+rsync -av --delete /mnt/data/unique/ /mnt/hdd/backups/unique
