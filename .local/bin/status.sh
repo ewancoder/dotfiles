9f603cec94bff54ba36570b485a30e16b743366f
Ivan Zyranau <ewancoder@gmail.com>
2025-05-13 02:58:09 +0400

    Make it possible to set torrent limits from a shortcut & show status

--- .local/bin/status.sh
+++ .local/bin/status.sh
@@ -7,11 +7,17 @@ while true; do
     date="$(date)"
     netstats="$(cat /tmp/netstats)"
     volume="$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')"
+    speedLimitsStatus="$(curl -s http://localhost:8080/api/v2/transfer/speedLimitsMode)"
+    if [ "$speedLimitsStatus" = "1" ]; then
+        speedLimits=$'\U0001F422'
+    else
+        speedLimits=''
+    fi
     sleep=0.2
 
     total_width=150
     left_part="$lyrics"
-    right_part="$netstats    🔊 $volume   $sensors   $date"
+    right_part="$speedLimits $netstats    🔊 $volume   $sensors   $date"
 
     left_width="${#left_part}"
     right_width="${#right_part}"
