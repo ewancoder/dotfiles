86de35283d5566f952de32637f09dab0a110a42d
Ivan Zyranau <ewancoder@gmail.com>
2025-09-26 23:10:23 +0400

    Add security folder to backup

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -7,3 +7,7 @@ rsync -av --delete /mnt/data/unique/ /mnt/backup/backups/unique
 rsync -av --delete /mnt/data/media/configs/ /mnt/hdd/backups/media-configs
 rsync -av --delete /mnt/data/seq/ /mnt/hdd/backups/seq
 rsync -av --delete /mnt/data/unique/ /mnt/hdd/backups/unique
+
+# ~/.gnupg and ~/.ssh folders, plus security keys
+rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security
+rsync -av --delete /mnt/data/security/ /mnt/hdd/backups/security
