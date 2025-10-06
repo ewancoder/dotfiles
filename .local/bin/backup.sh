293c308b683690e1e59bd0595432c22e53a22822
Ivan Zyranau <ewancoder@gmail.com>
2025-10-06 20:11:01 +0400

    Improve backups - use cloud for small data/configs

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
+# Run this from root only always.
 
-rsync -av --delete /mnt/data/media/configs/ /mnt/backup/backups/media-configs
-rsync -av --delete /mnt/data/seq/ /mnt/backup/backups/seq
 rsync -av --delete /mnt/data/unique/ /mnt/backup/backups/unique
+rsync -av --delete /mnt/data/tyrm/configs/ /mnt/backup/backups/tyrm-configs
+rsync -av --delete /mnt/data/tyr/ /mnt/backup/backups/tyr
 
-rsync -av --delete /mnt/data/media/configs/ /mnt/hdd/backups/media-configs
-rsync -av --delete /mnt/data/seq/ /mnt/hdd/backups/seq
 rsync -av --delete /mnt/data/unique/ /mnt/hdd/backups/unique
+rsync -av --delete /mnt/data/tyrm/configs/ /mnt/hdd/backups/tyrm-configs
+rsync -av --delete /mnt/data/tyr/ /mnt/backup/backups/tyr
 
 # ~/.gnupg and ~/.ssh folders, plus security keys
 rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security
 rsync -av --delete /mnt/data/security/ /mnt/hdd/backups/security
+
+# Backup tyrm configs
+cd /mnt/backup/backups
+rsync -av --delete /mnt/data/tyrm/configs/ /mnt/backup/backups/tyrm-configs-archive
+find tyrm-configs-archive -type d -name "logs" | xargs rm -r
+find tyrm-configs-archive -type d -name "log" | xargs rm -r
+rm -r "tyrm-configs-archive/jellyfin/data/data/subtitles"
+tar -czpf tyrm-configs-archive.tar.gz tyrm-configs-archive
+
+# Backup security
+cd /mnt/backup/backups
+rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security-archive
+tar -czpf security-archive.tar.gz security-archive
+
+# Backup to shared storage
+chown ewancoder:ewancoder *.tar.gz
+source /home/ewancoder/.secrets
+mkdir -p /tmp/crypt
+echo $CRYPT_PASSWORD | gocryptfs /mnt/data/Dropbox/backups /tmp/crypt
+cp *.tar.gz /tmp/crypt
+fusermount -u /tmp/crypt
