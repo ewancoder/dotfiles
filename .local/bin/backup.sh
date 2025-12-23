0444a698022918ff93dfcc1fae7f33d1119cd719
Ivan Zyranau <ewancoder@gmail.com>
2025-12-23 18:03:05 +0400

    Improve backups: split online/offline, create weekly security backups

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -17,6 +17,12 @@ rsync -av --delete /mnt/data/unique/ /mnt/backup/backups/unique
 # ~/.gnupg and ~/.ssh folders, plus important security keys
 rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security
 
+# Store weekly backups.
+rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security-weekly/security-$(date +%Y-W%U)
+
+# Store online-security part without device-specific things.
+rsync -av --delete /mnt/data/security/ /mnt/backup/backups/security-online --exclude='ssh' --exclude 'wg'
+
 # Development environment project data for TyR projects
 rsync -av --delete /mnt/data/tyr/ /mnt/backup/backups/tyr
 
@@ -28,7 +34,7 @@ rsync -av --delete /mnt/data/tyrm/configs/ /mnt/backup/backups/tyrm-configs --ex
 # Backup tyrm and security to shared storage
 cd /mnt/backup/backups
 tar -czpf "tyrm-configs-$(hostname).tar.gz" tyrm-configs
-tar -czpf "security-$(hostname).tar.gz" security
+tar -czpf "security-$(hostname).tar.gz" security-online
 
 mkdir -p /tmp/crypt
 echo $CRYPT_PASSWORD | gocryptfs /mnt/data/Dropbox/backups /tmp/crypt
