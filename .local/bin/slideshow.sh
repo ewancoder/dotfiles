422fd6e715cca457b4e4fe37c6edd778ef5b895d
Ivan Zyranau <ewancoder@gmail.com>
2026-02-17 00:33:44 +0400

    Put different wallpapers to different monitors

--- .local/bin/slideshow.sh
+++ .local/bin/slideshow.sh
@@ -5,7 +5,11 @@ WALLPAPER_DIR="/mnt/data/tyrm/screensaver"
 INTERVAL=600 # 10 minutes
 
 while true; do
-    wallpaper=$(find "$WALLPAPER_DIR/" -type f -name '*.jpg' -o -name '*.png' -o -name '*.webp' | shuf -n 1)
-    swaymsg "output * bg '$wallpaper' fill"
+    outputs=($(swaymsg -t get_outputs | jq -r '.[].name'))
+    for output in "${outputs[@]}"; do
+        wallpaper=$(find "$WALLPAPER_DIR/" -type f -name '*.jpg' -o -name '*.png' -o -name '*.webp' | shuf -n 1)
+        echo $output
+        swaymsg "output $output bg '$wallpaper' fill"
+    done
     sleep "$INTERVAL"
 done
