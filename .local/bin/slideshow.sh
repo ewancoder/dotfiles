97f7d4d7ad5bfee40f044cc1186bd8c59d98052c
Ivan Zyranau <ewancoder@gmail.com>
2026-02-20 08:43:01 +0400

    Fix slideshow script, when filenames contain spaces/brackets

--- .local/bin/slideshow.sh
+++ .local/bin/slideshow.sh
@@ -8,8 +8,10 @@ while true; do
     outputs=($(swaymsg -t get_outputs | jq -r '.[].name'))
     for output in "${outputs[@]}"; do
         wallpaper=$(find "$WALLPAPER_DIR/" -type f -name '*.jpg' -o -name '*.png' -o -name '*.webp' | shuf -n 1)
-        echo $output
-        swaymsg "output $output bg '$wallpaper' fill"
+        link="/tmp/wallpaper_$output"
+        ln -sf "$wallpaper" "$link"
+        swaymsg "output $output bg \"$link\" fill"
     done
+    echo "Updated at $(date)"
     sleep "$INTERVAL"
 done
