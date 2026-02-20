843d9349e4d229d5c9e8c2c0f14eb4ae958a82f5
Ivan Zyranau <ewancoder@gmail.com>
2026-02-20 08:42:36 +0400

    Use local account for RDP

--- .local/bin/rdpwin.sh
+++ .local/bin/rdpwin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pass=$(zenity --password)
-user="ewancoder@gmail.com"
+user="ewancoder"
 xfreerdp3 /audio-mode:0 /cert:ignore /auth-pkg-list:'!kerberos' /v:192.168.1.185 /u:"$user" /d:'' /p:"$pass" +compression +clipboard +fonts /dynamic-resolution +async-channels +async-update +auto-reconnect /bpp:16 /gfx:progressive /gfx /drive:linux,/mnt/data/rdp
